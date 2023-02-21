992f1847cc2a2bf0bd749d26ed00f255e5b32874
Mario Quevedo <opnfuso@gmail.com>
2023-02-20 19:47:00 -0600

    tooling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 const app = express();
 
 // Middleware que transforma la req.body en JSON
@@ -6,9 +6,9 @@ app.use(express.json());
 
 const PORT = 3000;
 
-app.get("/ping", (_req, res) => {
-  console.log("Alguien hizo ping");
-  res.send("pong");
+app.get('/ping', (_req, res) => {
+  console.log('Alguien hizo ping');
+  res.send('pong');
 });
 
 app.listen(PORT, () => {
