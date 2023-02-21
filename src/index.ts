7b78732c8e3949ca594014c68ed56a8a4a25e127
Mario Quevedo <opnfuso@gmail.com>
2023-02-20 19:39:28 -0600

    index

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+// Middleware que transforma la req.body en JSON
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/ping", (_req, res) => {
+  console.log("Alguien hizo ping");
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
