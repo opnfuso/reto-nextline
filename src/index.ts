96c88210ed449e0f9f9252d8e4ac69e0c7fb224e
Mario Quevedo <opnfuso@gmail.com>
2023-02-21 18:30:19 -0600

    extensions

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,17 @@ const app = express();
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger.json';
 
+import cors from 'cors';
+
 import { PORT } from './config';
 import tareasRoutes from './routes/tareas.routes';
+import helmet from 'helmet';
+
+// CORS
+app.use(cors);
+
+// Helmet
+app.use(helmet());
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
